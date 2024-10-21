0026f8b450b1b0db5499499e4d8df82cea5177e6
Arun silawat <arunsilawat722362@gmail.com>
2024-10-21 18:04:14 +0530

    Registration Page

--- project/app/urls.py
+++ project/app/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from app import views
 urlpatterns = [
-     path('', views.home, name='home')
+     path('', views.login, name='login'),
+     path('register/', views.register, name='register'),
 ]
